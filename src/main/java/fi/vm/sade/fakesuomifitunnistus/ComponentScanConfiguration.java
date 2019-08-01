bf3e5f6deef7f268a73cbd1aeb9d37bbc6c310a0
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-01 13:32:13 +0300

    Fix component auto configuration

---
+++ src/main/java/fi/vm/sade/fakesuomifitunnistus/ComponentScanConfiguration.java
@@ -0,0 +1,7 @@
+package fi.vm.sade.fakesuomifitunnistus;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("fi.vm.sade.fakesuomifitunnistus")
+public class ComponentScanConfiguration {
+}
