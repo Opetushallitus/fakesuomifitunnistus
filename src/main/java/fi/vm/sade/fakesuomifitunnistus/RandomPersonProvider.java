b23daf856e1781fa4d2822c5794b09ffe919bd72
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-08 15:40:14 +0300

    Add random person impl

--- src/main/java/fi/vm/sade/fakesuomifitunnistus/RandomPersonProvider.java
+++ src/main/java/fi/vm/sade/fakesuomifitunnistus/RandomPersonProvider.java
@@ -1,16 +1,25 @@
 package fi.vm.sade.fakesuomifitunnistus;
 
+import com.github.javafaker.Faker;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class RandomPersonProvider implements PersonProvider {
 
+    private final Faker faker;
+
+    public RandomPersonProvider() {
+        this.faker = new Faker(new Locale("fi-FI"));
+    }
+
     @Override
     public PersonDto get() {
         PersonDto person = new PersonDto();
-        person.surname = "";
-        person.firstNames = "";
-        person.preferredFirstName = "";
+        person.surname = faker.name().lastName();
+        person.firstNames = faker.name().firstName();
+        person.preferredFirstName = person.firstNames;
         return person;
     }
 
